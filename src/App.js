e4759e452bf69ad9ed03cd060643b4aaf491fdd5
Shivam Bharti <bhartishivam393@gmail.com>
2024-06-14 23:56:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Resturant from "./component/Basic/Resturant";
+
+const App = () => {
+  return (
+    <div>
+      <Resturant />
+    </div>
+  )
+}
+
+export default App;
